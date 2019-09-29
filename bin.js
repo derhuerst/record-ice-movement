ccf0c8390b341f52ade704dbe40cd4a8a26592d4
Jannis R <mail@jannisr.de>
2019-09-29 15:53:44 +0200

    don't exit on errors

--- bin.js
+++ bin.js
@@ -31,16 +31,13 @@ const showError = (err) => {
 	process.exit(1)
 }
 
-const pump = require('pump')
 const createStream = require('wifi-on-ice-position-stream')
 const {stringify} = require('ndjson')
 
-pump(
-	createStream(),
-	stringify(),
-	process.stdout,
-	(err) => {
-		console.error(err)
-		process.exit(1)
-	}
-)
+createStream()
+.on('error', (err) => {
+	console.error(err)
+	process.exitCode = 1
+})
+.pipe(stringify())
+.pipe(process.stdout)
